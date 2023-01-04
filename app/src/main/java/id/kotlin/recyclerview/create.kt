d8a903c70f3285c2c317301c4505d70741f00519
sazahi <103054130+sazahi@users.noreply.github.com>
2023-01-04 11:47:34 +0700

    Update create.kt

--- app/src/main/java/id/kotlin/recyclerview/create.kt
+++ app/src/main/java/id/kotlin/recyclerview/create.kt
@@ -4,26 +4,28 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Trace.isEnabled
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import id.kotlin.recyclerview.Api.RetrofitHelper
-import id.kotlin.recyclerview.Api.TodoApi
-import id.kotlin.recyclerview.Api.TodoData
+import com.example.sharedpref.api.RetrofitHelper
+import com.example.sharedpref.api.TodoApi
+import com.example.sharedpref.api.TodoData
 import kotlinx.android.synthetic.main.activity_create.*
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class
-create : AppCompatActivity() {
+class create : AppCompatActivity() {
 
     lateinit var etUsername: EditText
     lateinit var etDescription: EditText
 
-    val apiKey = ""
+    val apiKey =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impsc3N2cmV0cGJ6c25uYmZwaWpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA1ODMwMDUsImV4cCI6MTk4NjE1OTAwNX0.XzG93lsdOa4ap-NMu1cgquhdmywh1924-WnuM9f3fXs"
     val token = "Bearer $apiKey"
 
     val todoApi = RetrofitHelper.getInstance().create(TodoApi::class.java)
@@ -44,39 +46,36 @@ create : AppCompatActivity() {
         val sharedPreferences = getSharedPreferences(
             "app_preference", Context.MODE_PRIVATE
         )
-
-        btn_submit.isEnabled = false
-        etUsername.addTextChangedListener(object: TextWatcher {
-            override fun onTextChanged(s:CharSequence, start:Int, before:Int, count:Int) {
-                btn_submit.isEnabled = s.toString().trim{ it <= ' ' }.isNotEmpty()
-            }
-            override fun beforeTextChanged(s:CharSequence, start:Int, count:Int,
-                                           after:Int) {
-            }
-            override fun afterTextChanged(s: Editable) {
-            }
-        })
+        
 
         btn_submit.setOnClickListener {
-            CoroutineScope(Dispatchers.Main).launch {
-                val ssid = sharedPreferences.getString("session_id", "[No session ID found]").toString()
-                val data = TodoData(
-                    username = etUsername.text.toString(),
-                    description = etDescription.text.toString(),
-                    session_id = ssid
-                )
-                val response = todoApi.create(token = token, apiKey = apiKey, todoData = data)
+            if (etUsername.text.toString().isNullOrEmpty() || etDescription.text.toString().isNullOrEmpty()) {
+                Toast.makeText(this, "Isi Terlebih Dahulu", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else {
+                CoroutineScope(Dispatchers.Main).launch {
+                    val ssid = sharedPreferences.getString("session_id", "[No session ID found]")
+                        .toString()
+                    val data = TodoData(
+                        username = etUsername.text.toString(),
+                        description = etDescription.text.toString(),
+                        session_id = ssid
+                    )
+                    val response = todoApi.create(token = token, apiKey = apiKey, todoData = data)
 
-                Toast.makeText(
-                    applicationContext,
-                    "Posted!",
-                    Toast.LENGTH_SHORT
-                ).show()
+                    Toast.makeText(
+                        applicationContext,
+                        "Posted!",
+                        Toast.LENGTH_SHORT
+                    ).show()
 
-                finish()
+                    finish()
+                    btn_submit.isEnabled = false
+                    btn_submit.isClickable = false
+                }
             }
-            btn_submit.isEnabled = false
-            btn_submit.isClickable = false
+
+
         }
 
     }
